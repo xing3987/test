3dd03a425dfb785f46e3fbece950f4df112b8ac1
chenxing <xing3987@163.com>
2019-11-08 00:57:28 +0800

    Initial commit

---
+++ src/main/java/test/ErrorRecord.java
@@ -0,0 +1,126 @@
+package test;
+/*
+开发一个简单错误记录功能小模块，能够记录出错的代码所在的文件名称和行号。
+处理:
+1.记录最多8条错误记录，对相同的错误记录(即文件名称和行号完全匹配)只记录一条，错误计数增加；(文件所在的目录不同，文件名和行号相同也要合并)
+2.超过16个字符的文件名称，只记录文件的最后有效16个字符；(如果文件名不同，而只是文件名的后16个字符和行号相同，也不要合并)
+3.输入的文件可能带路径，记录文件名称不能带路径
+
+输入描述:
+一行或多行字符串。每行包括带路径文件名称，行号，以空格隔开。
+
+    文件路径为windows格式
+
+    如：E:\V1R2\product\fpgadrive.c 1325
+
+输出描述:
+将所有的记录统计并将结果输出，格式：文件名代码行数数目，一个空格隔开，如: fpgadrive.c 1325 1
+
+    结果根据数目从多到少排序，数目相同的情况下，按照输入第一次出现顺序排序。
+
+    如果超过8条记录，则只输出前8条记录.
+
+    如果文件名的长度超过16个字符，则只输出后16个字符
+
+输入例子1:
+E:\V1R2\product\fpgadrive.c 1325
+
+输出例子1:
+fpgadrive.c 1325 1
+*/
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class ErrorRecord {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        List<Record> records = new ArrayList<>();
+        while (sc.hasNext()) {
+            String path = sc.next();
+            String pathSimple = path.substring(path.lastIndexOf("\\") + 1);
+            int line = sc.nextInt();
+            Record record = new Record(pathSimple, line, 1);
+            insertIntoList(record, records);
+        }
+
+        Collections.sort(records);
+        int j = 0;
+        for (Record in : records) {
+            if (j++ == 8) {
+                break;
+            }
+            System.out.println(formatFileName(in.getFileName()) + " " + in.getLine() + " " + in.getCount());
+        }
+
+        sc.close();
+
+    }
+
+    private static String formatFileName(String fileName) {
+        if (fileName.length() > 16) {
+            return fileName.substring(fileName.length() - 16);
+        }
+        return fileName;
+    }
+
+    private static void insertIntoList(Record record, List<Record> records) {
+        for (Record in : records) {
+            if (record.getFileName().equals(in.getFileName()) && record.getLine() == in.getLine()) {
+                in.setCount(in.getCount() + 1);
+                return;
+            }
+        }
+        records.add(record);
+    }
+}
+
+
+class Record implements Comparable<Record> {
+    private String fileName;
+    private int line;
+    private int count;
+
+    public Record() {
+    }
+
+    public Record(String fileName, Integer line, Integer count) {
+        this.fileName = fileName;
+        this.line = line;
+        this.count = count;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public int getLine() {
+        return line;
+    }
+
+    public void setLine(int line) {
+        this.line = line;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    @Override
+    public int compareTo(Record o) {
+        return o.count - this.count;
+    }
+}
+
---
+++ src/main/java/test/StudentRecord.java
@@ -0,0 +1,74 @@
+package test;
+
+import java.util.Scanner;
+
+/*
+输入包括多组测试数据。
+每组输入第一行是两个正整数N和M（0 < N <= 30000,0 < M < 5000）,分别代表学生的数目和操作的数目。
+学生ID编号从1编到N。
+第二行包含N个整数，代表这N个学生的初始成绩，其中第i个数代表ID为i的学生的成绩
+接下来又M行，每一行有一个字符C（只取‘Q’或‘U’），和两个正整数A,B,当C为'Q'的时候, 表示这是一条询问操作，他询问ID从A到B（包括A,B）的学生当中，成绩最高的是多少
+当C为‘U’的时候，表示这是一条更新操作，要求把ID为A的学生的成绩更改为B。
+
+输出描述:
+对于每一次询问操作，在一行里面输出最高成绩.
+
+输入例子1:
+5 7
+1 2 3 4 5
+Q 1 5
+U 3 6
+Q 3 4
+Q 4 5
+U 4 5
+U 2 9
+Q 1 5
+
+输出例子1:
+5
+6
+5
+9
+* */
+public class StudentRecord {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String row = sc.nextLine();
+        String[] rows = row.split(" ");
+        int count = Integer.valueOf(rows[0]);
+        int control = Integer.valueOf(rows[1]);
+
+        String[] dataStr = sc.nextLine().split(" ");
+        Integer[] datas = new Integer[count];
+        for (int i = 0; i < dataStr.length; i++) {
+            datas[i] = Integer.valueOf(dataStr[i]);
+        }
+
+        for (int j = 0; j < control; j++) {
+            String detail=sc.nextLine();
+            String a = detail.split(" ")[0];
+            String b = detail.split(" ")[1];
+            String c = detail.split(" ")[2];
+            if (a.equals("Q")) {
+                System.out.println(getMax(datas, Integer.valueOf(b) - 1, Integer.valueOf(c) - 1));
+            } else {
+                reset(datas, Integer.valueOf(b) - 1, Integer.valueOf(c));
+            }
+        }
+        sc.close();
+    }
+
+    public static Integer getMax(Integer[] datas, int begin, int end) {
+        int max = datas[begin];
+        for (int i = begin; i <= end; i++) {
+            if (max < datas[i]) {
+                max = datas[i];
+            }
+        }
+        return max;
+    }
+
+    public static void reset(Integer[] datas, int index, int record) {
+        datas[index] = record;
+    }
+}
