3533275960322a4cec43352788dfeb0995919bb1
chenxing <xing3987@163.com>
2020-01-27 00:04:06 +0800

    leetcode

---
+++ src/main/java/leetcode/leetcode/editor/cn/P21MergeTwoSortedLists.java
@@ -0,0 +1,54 @@
+package leetcode.leetcode.editor.cn;
+//将两个有序链表合并为一个新的有序链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+//
+// 示例： 
+//
+// 输入：1->2->4, 1->3->4
+//输出：1->1->2->3->4->4
+// 
+// Related Topics 链表
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/*
+ * 合并两个有序链表,使用递归
+ */
+public class P21MergeTwoSortedLists {
+    public static void main(String[] args) {
+        Solution solution = new P21MergeTwoSortedLists().new Solution();
+    }
+
+    //Definition for singly-linked list.
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+            if (null == l1) {
+                return l2;
+            }
+            if (null == l2) {
+                return l1;
+            }
+            if (l1.val <= l2.val) {
+                //确定好了当前第一个，使用递归寻找下一个节点
+                l1.next = mergeTwoLists(l1.next, l2);
+                return l1;
+            } else {
+                l2.next = mergeTwoLists(l2.next, l1);
+                return l2;
+            }
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
