7ca7a2cf1c0e60ac65c2cab9e8f1063a9cba5b00
chenxing <xing3987@163.com>
2020-01-27 23:01:50 +0800

    leetcode

---
+++ src/main/java/leetcode/leetcode/editor/cn/P38CountAndSay.java
@@ -0,0 +1,77 @@
+package leetcode.leetcode.editor.cn;
+//「外观数列」是一个整数序列，从数字 1 开始，序列中的每一项都是对前一项的描述。前五项如下： 
+//
+// 1.     1
+//2.     11
+//3.     21
+//4.     1211
+//5.     111221
+// 
+//
+// 1 被读作 "one 1" ("一个一") , 即 11。 
+//11 被读作 "two 1s" ("两个一"）, 即 21。 
+//21 被读作 "one 2", "one 1" （"一个二" , "一个一") , 即 1211。 
+//
+// 给定一个正整数 n（1 ≤ n ≤ 30），输出外观数列的第 n 项。 
+//
+// 注意：整数序列中的每一项将表示为一个字符串。 
+//
+// 
+//
+// 示例 1: 
+//
+// 输入: 1
+//输出: "1"
+//解释：这是一个基本样例。 
+//
+// 示例 2: 
+//
+// 输入: 4
+//输出: "1211"
+//解释：当 n = 3 时，序列是 "21"，其中我们有 "2" 和 "1" 两组，"2" 可以读作 "12"，也就是出现频次 = 1 而 值 = 2；类似 
+//"1" 可以读作 "11"。所以答案是 "12" 和 "11" 组合在一起，也就是 "1211"。 
+// Related Topics 字符串
+
+/*
+外观数列
+*/
+public class P38CountAndSay {
+    public static void main(String[] args) {
+        Solution solution = new P38CountAndSay().new Solution();
+        System.out.println(solution.say("112222"));
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public String countAndSay(int n) {
+            String result = "1";
+            for (int i = 1; i < n; i++) {
+                result = say(result);
+            }
+            return result;
+        }
+
+        private String say(String n) {
+            char[] chars = n.toCharArray();
+            int m = 1;
+            StringBuffer sbf = new StringBuffer();
+            for (int i = 0; i < chars.length; i++) {
+                if (i == chars.length - 1) {
+                    sbf.append(m);
+                    sbf.append(chars[i]);
+                    break;
+                }
+                if (chars[i + 1] != chars[i]) {
+                    sbf.append(m);
+                    sbf.append(chars[i]);
+                    m = 1;
+                } else {
+                    m++;
+                }
+            }
+            return sbf.toString();
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
