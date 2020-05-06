dbcfaa970095f1b7fa1cac16fea501e32280e5ae
chenxing <xing3987@163.com>
2020-05-07 00:24:56 +0800

    leetcode

---
+++ src/main/java/leetcode/leetcode/editor/cn/P3LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,70 @@
+package leetcode.leetcode.editor.cn;
+//给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。 
+//
+// 示例 1: 
+//
+// 输入: "abcabcbb"
+//输出: 3 
+//解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+// 
+//
+// 示例 2: 
+//
+// 输入: "bbbbb"
+//输出: 1
+//解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+// 
+//
+// 示例 3: 
+//
+// 输入: "pwwkew"
+//输出: 3
+//解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+//     请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+// 
+// Related Topics 哈希表 双指针 字符串 Sliding Window
+
+import java.util.HashSet;
+import java.util.Set;
+
+/*
+无重复字符的最长子串
+*/
+public class P3LongestSubstringWithoutRepeatingCharacters {
+    public static void main(String[] args) {
+        Solution solution = new P3LongestSubstringWithoutRepeatingCharacters().new Solution();
+        solution.lengthOfLongestSubstring("au");
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int lengthOfLongestSubstring(String s) {
+            if (s.length() == 1) {
+                return 1;
+            }
+            int max = 0;
+            for (int i = 0; i < s.length(); i++) {
+                for (int j = i + 1; j <= s.length(); j++) {
+                    String str = s.substring(i, j);
+                    if (!hasRepeatChar(str)) {
+                        max = Math.max(str.length(), max);
+                    }
+                }
+            }
+            return max;
+        }
+
+        private boolean hasRepeatChar(String s) {
+            Set<Character> set = new HashSet<>();
+            for (int j = 0; j < s.length(); j++) {
+                boolean flag = set.add(s.charAt(j));
+                if (!flag) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
